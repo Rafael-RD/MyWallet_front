9fbe30a928eada3b026011d79ec81add785b5842
Rafael <rafaelreinertd@hotmail.com>
2023-04-23 19:40:21 -0300

    delete

--- src/pages/HomePage/Components/TransactionsContainer.js
+++ src/pages/HomePage/Components/TransactionsContainer.js
@@ -4,45 +4,53 @@ import { userInfo } from "../../../App";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-export default function TransactionsContainer(){
-    const {REACT_APP_API_URL}=process.env;
-    const [transactions, setTransactions]=useState([]);
-    const [total, setTotal]=useState(0);
-    const {user:{token}}=useContext(userInfo);
-    const navigate=useNavigate();
+export default function TransactionsContainer() {
+    const { REACT_APP_API_URL } = process.env;
+    const [transactions, setTransactions] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [reload, setReload]=useState(false);
+    const { user: { token } } = useContext(userInfo);
+    const navigate = useNavigate();
 
-    useEffect(()=>{
-
-        (async ()=>{
+    useEffect(() => {
+        setReload(false);
+        (async () => {
             try {
-                const resp=await axios.get(`${REACT_APP_API_URL}/transactions`,{ headers: { Authorization: `Bearer ${token}` } });
-                let auxtotal=0;
-                resp.data.forEach(e=>{
-                    if(e.type==='inbound') auxtotal+=e.value;
-                    else if(e.type==='outbound') auxtotal-=e.value;
+                const resp = await axios.get(`${REACT_APP_API_URL}/transactions`, { headers: { Authorization: `Bearer ${token}` } });
+                let auxtotal = 0;
+                resp.data.forEach(e => {
+                    if (e.type === 'inbound') auxtotal += e.value;
+                    else if (e.type === 'outbound') auxtotal -= e.value;
                 });
                 setTotal(auxtotal);
                 setTransactions(resp.data);
             } catch (error) {
-                if(error.response.status===401) navigate('/');
+                if (error.response.status === 401) navigate('/');
                 else console.log(error);
             }
         })()
-    },[])
+    }, [reload])
 
-    function deleteItem(id){
-        alert('Função ainda não implementada')
-        console.log(id);
+    async function deleteItem(id) {
+        const confirmation = window.confirm('Deseja realmente excluir esse item?');
+        try {
+            const respDelete =await axios.delete(`${REACT_APP_API_URL}/transactions`, {data: {id}, headers: { Authorization: `Bearer ${token}` } });
+            if(respDelete.status===200){
+                setReload(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    function showTransactions(){
-        if(transactions.length===0) return <span>Não há registros de<br />entrada ou saída</span>;
+    function showTransactions() {
+        if (transactions.length === 0) return <span>Não há registros de<br />entrada ou saída</span>;
         else return (
             <ul>
-                {transactions.map(e=>{
-                    return(
+                {transactions.map(e => {
+                    return (
                         <ListItem key={e._id} type={e.type}>
-                            <span>{`${(new Date(e.timestamp).getDate())}/${(new Date(e.timestamp).getMonth())}`}</span><div>{e.description}<span>{e.value.toFixed(2).replace('.',',')}</span></div><button onClick={()=>deleteItem(e._id)}>X</button>
+                            <span>{`${(new Date(e.timestamp).getDate())}/${(new Date(e.timestamp).getMonth())}`}</span><div>{e.description}<span>{e.value.toFixed(2).replace('.', ',')}</span></div><button onClick={() => deleteItem(e._id)}>X</button>
                         </ListItem>
                     )
                 })}
@@ -50,12 +58,12 @@ export default function TransactionsContainer(){
         )
     }
 
-    return(
+    return (
         <Container>
             {showTransactions()}
-            <Total negative={total<0} hide={transactions.length===0}>
+            <Total negative={total < 0} hide={transactions.length === 0}>
                 SALDO
-                <span>{total.toFixed(2).replace('.',',')}</span>
+                <span>{total.toFixed(2).replace('.', ',')}</span>
             </Total>
         </Container>
     )
@@ -89,7 +97,7 @@ const Container = styled.div`
 
 `;
 
-const ListItem=styled.li`
+const ListItem = styled.li`
     display: flex;
     width: 100%;
     margin-bottom: 20px;
@@ -129,13 +137,13 @@ const ListItem=styled.li`
             font-style: normal;
             font-weight: 400;
             font-size: 16px;
-            color: ${props=>props.type==='inbound'?'#03AC00':'#C70000'};
+            color: ${props => props.type === 'inbound' ? '#03AC00' : '#C70000'};
         }
     }
     `;
 
-const Total=styled.div`
-    display: ${props=>props.hide?'none':'flex'};
+const Total = styled.div`
+    display: ${props => props.hide ? 'none' : 'flex'};
     align-items: center;
     justify-content: space-between;
     width: 100%;
@@ -146,6 +154,6 @@ const Total=styled.div`
 
     span{
         font-weight: 400;
-        color: ${props=>props.negative?'#C70000':'#03AC00'};
+        color: ${props => props.negative ? '#C70000' : '#03AC00'};
     }
 `;
